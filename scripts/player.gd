04f79dbf450c7fdd7bbff2f6b15fefeadd04c22d
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-06-27 19:33:59 +0200

    starting to redo sliding again

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ var slide_speed = 0
 var falling = false
 var fall_distance = 0
 var sliding = false
+var slide_distance : float
 
 
 var stamina = 100
@@ -71,38 +72,20 @@ func _physics_process(delta):
 	#slide
 	
 	if sliding:
-		slide_speed -= 2
+		if get_floor_angle() > 0.2:
+			slide_speed *= get_floor_angle()
 	
-	if falling and is_on_floor() and sliding:
-		slide_speed += fall_distance / 10
-	fall_distance = -gravity_vec.y
+	#slide_speed = slide_speed * 2
+	
 	
 	if Input.is_action_just_pressed("slide") and SPEED > 3:
 		can_slide = true
-	
-	if Input.is_action_pressed("slide") and is_on_floor() and Input.is_action_pressed("w") and can_slide == true:
 		slide()
-		slide_speed -= 1
 		
 	if Input.is_action_just_released("slide"):
 		state = RUN
 		can_slide = false
 		sliding = false
-		
-	var accel = acceleration if is_on_floor() else air_acceleration
-	if sliding:
-		accel = 1
-	else: accel = 0
-	
-	if sliding and get_floor_angle() > 0.2:
-		accel = 2
-	
-	slide_speed * accel
-	
-	if get_floor_angle() >= 0.2:
-		slide_speed += 1
-	
-	
 	
 	health_bar.value = health
 	stamina_bar.value = stamina
@@ -120,10 +103,8 @@ func _physics_process(delta):
 	else:
 		falling = false
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		if sliding:
-			slide_speed -= 1
 		rotation.x = 0
-		floor_stop_on_slope = false
+		floor_stop_on_slope = true
 		velocity.y = JUMP_VELOCITY
 	
 	
@@ -147,34 +128,32 @@ func run_state():
 	if not sliding:
 		scale.y = 1
 		floor_stop_on_slope = true
-	
+	slide_speed = 0
 	SPEED = 7.0
 	var input_dir = Input.get_vector("a", "d", "w", "s")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		var speed_tween = get_tree().create_tween()
-		speed_tween.tween_property(self, "SPEED", 7.0, 1)
-		if !$Neck/Camera3D/hand/Gun/AnimationPlayer.is_playing():
-			$Neck/Camera3D/hand/Gun/AnimationPlayer.play("Run")
+	if not sliding:
+		if direction:
+			var speed_tween = get_tree().create_tween()
+			speed_tween.tween_property(self, "SPEED", 7.0, 1)
+			if !$Neck/Camera3D/hand/Gun/AnimationPlayer.is_playing():
+				$Neck/Camera3D/hand/Gun/AnimationPlayer.play("Run")
+				
+			var runFovTween = get_tree().create_tween()
+			runFovTween.tween_property($Neck/Camera3D, "fov", 100, 0.5)
 			
-		var runFovTween = get_tree().create_tween()
-		runFovTween.tween_property($Neck/Camera3D, "fov", 100, 0.5)
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			if dead == false:
+				var idlefovTween = get_tree().create_tween()
+				idlefovTween.tween_property($Neck/Camera3D, "fov", 90, 0.5)
+			SPEED = 7.0
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 		
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		if dead == false:
-			var idlefovTween = get_tree().create_tween()
-			idlefovTween.tween_property($Neck/Camera3D, "fov", 90, 0.5)
-		SPEED = 7.0
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	if Input.is_action_just_pressed("sprint"):
-		state = SPRINTING
-		
-	if Input.is_action_just_pressed("slide") or Input.is_action_just_pressed("slide2"):
-		state = SLIDE
+		if Input.is_action_just_pressed("sprint"):
+			state = SPRINTING
 	
 		
 		
@@ -198,29 +177,31 @@ func shoot_state():
 		canShoot = true
 		
 func sprint_state():
-	SPEED = 9.0
-	stamina -= 1
-	
-	var input_dir = Input.get_vector("a", "d", "w", "s")
-	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		if !$Neck/Camera3D/hand/Gun/AnimationPlayer.is_playing():
-			$Neck/Camera3D/hand/Gun/AnimationPlayer.play("Run")
-		var sprintFovTween = get_tree().create_tween()
-		sprintFovTween.tween_property($Neck/Camera3D, "fov", 110, 0.5)
+	slide_speed = 9.0
+	if not sliding:
+		SPEED = 9.0
+		stamina -= 1
 		
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		SPEED = 7.0
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-	if Input.is_action_just_released("sprint"):
-		state = RUN
-		
-		await get_tree().create_timer(1).timeout
-		stamina_regen = true
+		var input_dir = Input.get_vector("a", "d", "w", "s")
+		var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			if !$Neck/Camera3D/hand/Gun/AnimationPlayer.is_playing():
+				$Neck/Camera3D/hand/Gun/AnimationPlayer.play("Run")
+			var sprintFovTween = get_tree().create_tween()
+			sprintFovTween.tween_property($Neck/Camera3D, "fov", 110, 0.5)
+			
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			SPEED = 7.0
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+			
+		if Input.is_action_just_released("sprint"):
+			state = RUN
+			
+			await get_tree().create_timer(1).timeout
+			stamina_regen = true
 		
 	if stamina <= 0:
 		state = RUN
@@ -245,31 +226,19 @@ func _on_stamina_timer_timeout():
 		stamina += 1
 		
 func slide():
-	if not sliding:
-		if slidecheck.is_colliding() or get_floor_angle() < 0.2:
-			floor_stop_on_slope = false
-			scale.y = 0.5
-			slide_speed = 5 + SPEED
-			slide_speed += fall_distance / 5
-		else:
-			slide_speed = 2
-	
-	print("speed: " + str(slide_speed))
-	
-	if slidecheck.is_colliding():
-		slide_speed += get_floor_angle() / 10
-	else:
-		slide_speed -= (get_floor_angle() / 5) + 0.03
-		
-	if slide_speed >= 30:
-		slide_speed = 30
-		
-	"""var slide_speed_tween = get_tree().create_tween()
-	slide_speed_tween.tween_property(self, "slide_speed", 0, 2.0)"""
-	
-	if slide_speed <= 0:
-		can_slide = false
-		sliding = false
-	SPEED = slide_speed
+	if can_slide:
+		SPEED = 0
+		slide_speed = 0
+		sliding = true
+	if sliding == true and can_slide == true:
+		print(get_floor_angle())
+		scale.y = 0.5
+		floor_stop_on_slope = false
+		position = position.lerp(position * slide_distance, slide_speed)
+		print(slide_speed)
+			
+	if slide_speed > 40:
+		slide_speed = 40
+
 
 
