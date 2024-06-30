13d20c0a8b9ebec187c3ae9c36abe7a74f76e5ac
Oliver <145380943+OliverSkogsrud@users.noreply.github.com>
2024-06-30 16:02:52 +0200

    HUGE bug fix

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -19,7 +19,7 @@ func _process(delta):
 	if $RayCast3D.is_colliding():
 		var collider = $RayCast3D.get_collider()
 		if collider.has_method("damage"):
-			collider.damage(damage)
+			get_parent().get_node(collider.get_path()).damage(20)
 		else:
 			particles.emitting = true
 			await get_tree().create_timer(1.0).timeout
